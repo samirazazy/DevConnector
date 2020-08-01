fecfc8a5f4f2459f440d2b2ecbaa5a72681c253b
samirazazy <samiralazazy@gmail.com>
2020-08-01 16:49:55 +0200

    7. Redux Setup & Alerts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
-const App = () => <Fragment className="App">App</Fragment>;
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
