b8c13d2451da6761fbabea6aa894dfaa3d4d82ed
samirazazy <samiralazazy@gmail.com>
2020-08-06 21:17:02 +0200

    12. Prepare & Deploy

--- server.js
+++ server.js
@@ -10,13 +10,23 @@ connectDB();
 app.use(express.json());
 
 // test API routes
-app.get('/', (req, res) => res.send('API runing'));
+// app.get('/', (req, res) => res.send('API runing'));
 
 // define routes
 app.use('/api/users', require('./routes/api/users.js'));
 app.use('/api/profile', require('./routes/api/profile.js'));
 app.use('/api/posts', require('./routes/api/posts.js'));
 app.use('/api/auth', require('./routes/api/auth.js'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => `server started on port ${PORT} 🎉🎉🎉`);
