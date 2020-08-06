436ccbc8f40acf1431150554021360802861ae25
samirazazy <samiralazazy@gmail.com>
2020-08-06 23:44:22 +0200

    deployment setup

---
+++ client/build/precache-manifest.401d9152f688cb0e0f0548c37a3f679f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59b7412c09ac48d4213efa33705e74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4be008a393c7718780",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "72162a050aa4f343d7b1",
+    "url": "/static/js/2.92549983.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.92549983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4be008a393c7718780",
+    "url": "/static/js/main.aa3ec651.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
