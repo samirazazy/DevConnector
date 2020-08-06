7beb37834302e30eb97b417bf70f4299dc1a3f00
samirazazy <samiralazazy@gmail.com>
2020-08-06 21:04:59 +0200

    9. Dashboard & Profile Management

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <section className="container">
-          <Alert />
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Switch>
-            <Route exact path="/" component={Register} />
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
           </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,79 +1,64 @@
-import api from '../utils/api';
-import { setAlert } from './alert';
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL,
+  //REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT
-} from './types';
+  //LOGIN_FAIL,
+  LOGOUT,
+  ACCOUNT_DELETED
+} from '../actions/types';
 
-// Load User
-export const loadUser = () => async (dispatch) => {
-  try {
-    const res = await api.get('/auth');
-
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR
-    });
-  }
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null
 };
 
-// Register User
-export const register = (formData) => async (dispatch) => {
-  try {
-    const res = await api.post('/users', formData);
+export default function (state = initialState, action) {
+  const { type, payload } = action;
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-    }
-
-    dispatch({
-      type: REGISTER_FAIL
-    });
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case ACCOUNT_DELETED:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null
+      };
+    case AUTH_ERROR:
+    case LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null
+      };
+    default:
+      return state;
   }
-};
-
-// Login User
-export const login = (email, password) => async (dispatch) => {
-  const body = { email, password };
-
-  try {
-    const res = await api.post('/auth', body);
-
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data
-    });
-
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-    }
-
-    dispatch({
-      type: LOGIN_FAIL
-    });
-  }
-};
-
-// Logout
-export const logout = () => ({ type: LOGOUT });
+}
